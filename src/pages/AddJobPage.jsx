b6b4b0029cb5455b243a92f675219dc1f167be3e
rash4p4 <rash4p4@gmail.com>
2026-01-08 16:57:50 +0530

    Change currency symbol from $ to  in data and forms

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -7,7 +7,7 @@ const AddJobPage = ({ addJobSubmit }) => {
   const [type, setType] = useState('Full-Time');
   const [location, setLocation] = useState('');
   const [description, setDescription] = useState('');
-  const [salary, setSalary] = useState('Under $50K');
+  const [salary, setSalary] = useState('Under ₹50K');
   const [companyName, setCompanyName] = useState('');
   const [companyDescription, setCompanyDescription] = useState('');
   const [contactEmail, setContactEmail] = useState('');
@@ -250,17 +250,17 @@ const AddJobPage = ({ addJobSubmit }) => {
                 value={salary}
                 onChange={(e) => setSalary(e.target.value)}
               >
-                <option value='Under 50K'>Under 50K</option>
-                <option value='50K  - 60K'>50K - 60K</option>
-                <option value='60K - 70K'>60K - 70K</option>
-                <option value='70K - 80K'>70K - 80K</option>
-                <option value='80K - 90K'>80K - 90K</option>
-                <option value='90K - 100K'>90K - 100K</option>
-                <option value='100K - 125K'>100K - 125K</option>
-                <option value='125K - 150K'>125K - 150K</option>
-                <option value='150K - 175K'>150K - 175K</option>
-                <option value='175K - 200K'>175K - 200K</option>
-                <option value='Over 200K'>Over 200K</option>
+                <option value='Under ₹50K'>Under ₹50K</option>
+                <option value='₹50K - ₹60K'>₹50K - ₹60K</option>
+                <option value='₹60K - ₹70K'>₹60K - ₹70K</option>
+                <option value='₹70K - ₹80K'>₹70K - ₹80K</option>
+                <option value='₹80K - ₹90K'>₹80K - ₹90K</option>
+                <option value='₹90K - ₹100K'>₹90K - ₹100K</option>
+                <option value='₹100K - ₹125K'>₹100K - ₹125K</option>
+                <option value='₹125K - ₹150K'>₹125K - ₹150K</option>
+                <option value='₹150K - ₹175K'>₹150K - ₹175K</option>
+                <option value='₹175K - ₹200K'>₹175K - ₹200K</option>
+                <option value='Over ₹200K'>Over ₹200K</option>
               </select>
             </div>
 
